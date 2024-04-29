31c177472dbdf9c2478e11ce9ed4405cc595da21
Ab7in <102387877+Ab7in@users.noreply.github.com>
2024-04-29 22:36:18 +0330

    Add files via upload

---
+++ chess.js
@@ -0,0 +1,13 @@
+// Create a new element for the message
+const message = document.createElement('div');
+message.textContent = 'Made by Noormohammadi And joharkesh';
+message.style.color = '000000'
+message.style.position = 'fixed';
+message.style.bottom = '10px';
+message.style.left = '10px';
+message.style.backgroundColor = '#ffffff';
+message.style.padding = '10px';
+message.style.border = '1px solid #ccc';
+
+// Append the message to the body of the webpage
+document.body.appendChild(message);
